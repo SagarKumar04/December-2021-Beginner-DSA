e7ec668ce5cce148b91952dfcf1ce9e954780d59
SagarKumar04 <sagarsngh93@gmail.com>
2022-01-07 22:54:35 +0530

    2-D Arrays

---
+++ src/Arrays/OneDLengthExample.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class OneDLengthExample {
+    public static void main(String[] args) {
+        int arr[] = new int[199];
+        int length = arr.length;
+
+        System.out.println("Length: " + length);
+    }
+}
---
+++ src/Arrays/TwoDLengthExample.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class TwoDLengthExample {
+    public static void main(String[] args) {
+        int arr[][] = new int[9][5];
+        int row = arr.length;
+        int column = arr[0].length;
+
+        System.out.println("Number of Rows: " + row);
+        System.out.println("Number of Columns: " + column);
+    }
+}
