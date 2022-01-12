6a50c9ea5be425c8873c0fc36f036295633af00a
SagarKumar04 <sagarsngh93@gmail.com>
2022-01-12 23:13:27 +0530

    Binary Search, Ternary Search

---
+++ src/OverflowExample.java
@@ -0,0 +1,7 @@
+public class OverflowExample {
+    public static void main(String[] args) {
+        //Range of byte: -128 to 127
+        byte b = (byte) 500;
+        System.out.println(b);
+    }
+}
