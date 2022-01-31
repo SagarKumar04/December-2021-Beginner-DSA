20f8871fba80c59970a4e3a7193cc51936e649a6
SagarKumar04 <sagarsngh93@gmail.com>
2022-01-31 23:17:09 +0530

    String

---
+++ src/String/StringBuffer/Insert.java
@@ -0,0 +1,13 @@
+package String.StringBuffer;
+
+public class Insert {
+    public static void main(String[] args) {
+        StringBuffer stringBuffer = new StringBuffer("This sparta!");
+
+        System.out.println("Initial: " + stringBuffer);
+
+        stringBuffer.insert(5, "is ");
+
+        System.out.println("Final: " + stringBuffer);
+    }
+}
