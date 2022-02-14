7d378e48b86fb0fd551a099999382a18c72c89d6
SagarKumar04 <sagarsngh93@gmail.com>
2022-02-14 23:11:24 +0530

    Singly Linked List

---
+++ src/LinkedList/DeleteMiddleElementInLinkedList.java
@@ -0,0 +1,35 @@
+package LinkedList;
+
+public class DeleteMiddleElementInLinkedList {
+    class Node {
+        Node next;
+        int val;
+
+        Node(int val) {
+            this.val = val;
+            next = null;
+        }
+    }
+
+    public static Node deleteMiddleElement(Node head) {
+        if(head.next==null) {
+            head.val = -1;
+        }
+        else {
+            Node slow = head;
+            Node fast = head;
+
+            while(fast.next.next != null
+                    && fast.next.next.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            Node delete = slow.next;
+            slow.next = delete.next;
+            delete.next = null;
+        }
+
+        return head;
+    }
+}
--- src/LinkedList/SinglyLinkedList.java
+++ src/LinkedList/SinglyLinkedList.java
@@ -3,6 +3,7 @@ package LinkedList;
 public class SinglyLinkedList {
     Node head;
     Node tail;
+    int size;
 
     static class Node {
         int data;
@@ -16,6 +17,7 @@ public class SinglyLinkedList {
 
     //traversing the list
     private void traverse() {
+        System.out.println("Current size: " + size);
         if(head == null) {
             System.out.println("List is empty");
         }
@@ -35,8 +37,49 @@ public class SinglyLinkedList {
         }
     }
 
+    //insert a node at the beginning of the linked list
+    private void insertAtStart(int data) {
+        Node newNode = new Node(data);
+        if (head != null) {
+            newNode.next = head;
+        }
+        head = newNode;
+
+        size++;
+    }
+
+    //insert the new node at the given position
+    private void insertAtPosition(int data, int position) {
+        if(position < 1 || position > (size + 1)) {
+            System.out.println("Invalid position");
+        }
+        else {
+            if(position == 1) {
+                insertAtStart(data);
+            }
+            else if(position == (size + 1)) {
+                insertAtEnd(data);
+            }
+            else {
+                Node temp = head;
+                int currentPosition = 1;
+
+                while(currentPosition != (position - 1)) {
+                    temp = temp.next;
+                    currentPosition++;
+                }
+
+                Node newNode = new Node(data);
+                newNode.next = temp.next;
+                temp.next = newNode;
+
+                size++;
+            }
+        }
+    }
+
     //insert the new node at the end of the linked list
-    private void push(int data) {
+    private void insertAtEnd(int data) {
         Node newNode = new Node(data);
 
         if(head == null) {
@@ -46,16 +89,69 @@ public class SinglyLinkedList {
             tail.next = newNode;
         }
         tail = newNode;
+
+        size++;
+    }
+
+    private void delete(int position) {
+        if(position < 1 || position > size) {
+            System.out.println("Invalid position");
+        }
+        else {
+            Node temp = head;
+            if(position == 1) {
+                head = head.next;
+                temp.next = null;
+            }
+            else {
+                int currentPosition = 1;
+
+                while(currentPosition != (position - 1)) {
+                    temp = temp.next;
+                    currentPosition++;
+                }
+
+                Node delete = temp.next;
+                temp.next = delete.next;
+                delete.next = null;
+
+                if(temp.next == null) {
+                    tail = temp;
+                }
+            }
+            size--;
+        }
     }
 
     public static void main(String[] args) {
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
         singlyLinkedList.traverse();
 
-        singlyLinkedList.push(8);
-        singlyLinkedList.push(-10);
-        singlyLinkedList.push(20);
+        singlyLinkedList.insertAtEnd(8);
+        singlyLinkedList.insertAtEnd(-10);
+        singlyLinkedList.insertAtEnd(20);
+        singlyLinkedList.traverse();
 
+        singlyLinkedList.insertAtStart(-100);
+        singlyLinkedList.insertAtStart(-50);
+        singlyLinkedList.traverse();
+
+        singlyLinkedList.insertAtPosition(1000, -2);
+        singlyLinkedList.insertAtPosition(2000, 0);
+        singlyLinkedList.insertAtPosition(3000, 7);
+        singlyLinkedList.insertAtPosition(10000, 3);
+        singlyLinkedList.traverse();
+        singlyLinkedList.insertAtPosition(20000, 1);
+        singlyLinkedList.traverse();
+        singlyLinkedList.insertAtPosition(30000, 8);
+
+        singlyLinkedList.traverse();
+
+        singlyLinkedList.delete(1);
+        singlyLinkedList.traverse();
+        singlyLinkedList.delete(7);
+        singlyLinkedList.traverse();
+        singlyLinkedList.delete(4);
         singlyLinkedList.traverse();
     }
 }
