35c7ffcf4fb160037ad8fdb5c28bc31cee5149e2
SagarKumar04 <sagarsngh93@gmail.com>
2022-08-11 20:43:03 +0530

    Removed Scanner and used BufferedReader in Boolean Matrix Problem

--- src/Revision/August7/BooleanMatrixProblemUsing1DArray.java
+++ src/Revision/August7/BooleanMatrixProblemUsing1DArray.java
@@ -1,23 +1,29 @@
 package Revision.August7;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class BooleanMatrixProblemUsing1DArray {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader
+                = new BufferedReader(new InputStreamReader(System.in));
 
-        int t = scanner.nextInt();
+        int t = Integer.parseInt(bufferedReader.readLine());
 
         for(int i = 0; i < t; i++) {
-            int m = scanner.nextInt();
-            int n = scanner.nextInt();
+            String mAndN = bufferedReader.readLine();
+            int m = Integer.parseInt(mAndN.split(" ")[0]);
+            int n = Integer.parseInt(mAndN.split(" ")[1]);
 
             int eachRow[] = new int[n];
 
             for(int j = 0; j < m; j++) {
                 boolean containsOne = false;
+                String row = bufferedReader.readLine();
+                String rowValues[] = row.split(" ");
                 for(int k = 0; k < n; k++) {
-                    eachRow[k] = scanner.nextInt();
+                    eachRow[k] = Integer.parseInt(rowValues[k]);
 
                     if(eachRow[k] == 1) {
                         containsOne = true;
